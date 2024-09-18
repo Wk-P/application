adcb15ede8f9d2ac3bb71ddb5d556161290dfa6b
Wk-P <weekend2000@gmail.com>
2024-09-18 18:09:48 +0900

    update files

--- VueProject/vite.config.ts
+++ VueProject/vite.config.ts
@@ -5,8 +5,31 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import os from 'node:os'; // 引入 os 模块
 
+
+// 获取本地网络接口中的私有 IP 地址
+function getLocalIPAddress(): string {
+    const nets = os.networkInterfaces();
+    for (const name of Object.keys(nets)) {
+        const netInterfaces = nets[name];
+        if (netInterfaces) {
+            for (const net of netInterfaces) {
+                // 过滤出 IPv4 地址，并且是私有地址（192.x.x.x）
+                if (net.family === 'IPv4' && net.address.startsWith('192.')) {
+                    return net.address;
+                }
+            }
+        }
+    }
+    return 'localhost'; // 默认值
+}
+
 const currentPlatform = os.platform(); // 获取当前平台
-const serverHost = currentPlatform === 'win32' ? '192.168.1.6' : 'localhost';
+let serverHost = getLocalIPAddress();
+
+if (currentPlatform === 'linux') {
+    serverHost = '127.0.0.1';
+}
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
