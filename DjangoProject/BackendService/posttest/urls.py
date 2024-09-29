824f64039ab402e4c95c7b7d6b6e02da52765030
Wk-P <weekend2000119@gmail.com>
2024-09-29 19:34:06 +0900

    update files

--- DjangoProject/BackendService/posttest/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from posttest.views import PostTest
-
-urlpatterns = [
-    path("", PostTest.as_view()),
-]
--- DjangoProject/BackendService/posttest/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework.views import APIView
-# Create your views here.
-
-
-class PostTest(APIView):
-    def get(self, request):
-        return Response({"success": True, "message": "OK"}, status=200)
