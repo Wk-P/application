1767ece75f6f7702cd13bb0f893517573b600e14
Wk-P <weekend2000@gmail.com>
2024-10-03 18:29:09 +0900

    update files

--- windows-scripts/start-django.ps1
+++ windows-scripts/start-django.ps1
@@ -5,12 +5,16 @@ $managePyDir = "$appDir\DjangoProject\BackendService"
 # Navigate to the Django project root directory
 Set-Location "$appDir\DjangoProject"
 
-# Activate virtual environment (if applicable)
-.\.venv\Scripts\Activate
+# Check if the virtual environment exists and activate it
+if (Test-Path "$appDir\DjangoProject\.venv\Scripts\Activate") {
+    .\.venv\Scripts\Activate
+    Write-Host "Virtual environment activated."
+} else {
+    Write-Host "Virtual environment not found. Please create one first."
+    exit
+}
 
-Set-Location "$appDir"
-
-# Navigate to the Django project's BackendService directory
+Set-Location "$managePyDir"
 
 # Remove the existing SQLite database file (if exists)
 if (Test-Path "db.sqlite3") {
